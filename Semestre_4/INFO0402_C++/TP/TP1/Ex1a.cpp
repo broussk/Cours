737b1f221461f51de27febb63c2d77cab5e94324
tonn0005 <nathan.tonnelle@etudiant.univ-reims.fr>
2020-01-14 08:05:59 +0100

    add cours

--- Semestre_4/INFO0402_C++/TP/TP1/Ex1a.cpp
+++ Semestre_4/INFO0402_C++/TP/TP1/Ex1a.cpp
@@ -6,7 +6,7 @@ int fun1a(int  x) {
 	return x;
 }
 
-int fun1b(int &x) { 
+int fun1b(int &x) {
 	x++;
 	return x;
 }
@@ -23,42 +23,43 @@ int fun1d(int *x) {
 
 int main() {
 	// constante
-	fun1a(4);
-	fun1b(4);
-	fun1c(4);
-	fun1d(&4);
+	printf("\nfun1a(4)=%d",fun1a(4));
+	//fun1a(4);
+	//fun1b(4);
+	printf("\nfun1c(4)=%d",fun1c(4));
+	//fun1d(&4);
 	
 	// variable sans modificateur
 	int i = 4;
-	fun1a(i);
-	fun1b(i);
-	fun1c(i);
-	fun1d(&i);
+	printf("\n\nfun1a(i=4)=%d (i=%d)",fun1a(i),i);
+	printf("\nfun1b(i=4)=%d (i=%d)",fun1b(i),i);
+	//fun1c(i);
+	printf("\nfun1d(i=4)=%d (i=%d)",fun1d(&i),i);
 	
 	// variable non modifiée
 	const int j = 4;
-	fun1a(j);
-	fun1b(j);
-	fun1c(j);
-	fun1d(&j);
+	printf("\n\nfun1a(j=4)=%d (j=%d)",fun1a(j),j);
+	//fun1b(j);
+	//fun1c(j);
+	//fun1d(&j);
 	
 	// référence sur une lvalue
 	int &k = i;
-	fun1a(k);
-	fun1b(k);
-	fun1c(k);
-	fun1d(&k);
+	printf("\n\nfun1a(k)=%d (k=%d)",fun1a(k),k);
+	printf("\nfun1b(k)=%d (k=%d)",fun1b(k),k);
+	//fun1c(k);
+	printf("\nfun1d(k)=%d (k=%d)",fun1d(&k),k);
 	
 	// référence sur une lvalue
 	const int &m = i;
-	fun1a(m);
-	fun1b(m);
-	fun1c(m);
-	fun1d(&m);
+	printf("\n\nfun1a(m)=%d (m=%d)",fun1a(m),m);
+	//fun1b(m);
+	//fun1c(m);
+	//fun1d(&m);
 
 	// référence sur une rvalue
-	fun1a(i+4);
-	fun1b(i+4);
-	fun1c(i+4);
-	fun1d(&(i+4));	
+	printf("\n\nfun1a(i+4)=%d (i=%d)",fun1a(i+4),i);
+	//fun1b(i+4);
+	printf("\nfun1c(i+4)=%d (i=%d)",fun1c(i+4),i);
+	//fun1d(&(i+4));
 }
