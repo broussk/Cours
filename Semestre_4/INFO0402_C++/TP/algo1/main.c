7474c63b04689eae68ff801d9f6372bea505c266
tonn0005 <nathan.tonnelle@etudiant.univ-reims.fr>
2020-01-15 21:25:21 +0100

    add cours

--- Semestre_4/INFO0402_C++/TP/algo1/main.c
+++ Semestre_4/INFO0402_C++/TP/algo1/main.c
@@ -2,9 +2,88 @@
 
 int main( )
 {
-    printf("Hello, World!\n");
-    int m1[][],m2[][],m3[][];
+    //initialisation des variables usuelles
     int taille, i, j;
-    printf("entrez une taille ")
+
+    //titre du programme
+    printf("Calcul du maximum de 2 images binaires\n");
+
+    //initialisation des matrices carrées
+    printf("entrez une taille des matrice carree\n");
+    scanf("%d",&taille);
+    int M1[taille][taille],M2[taille][taille],M3[taille][taille];
+
+    //initialisation de la matrice M1
+    printf("entrez les valeurs de la matrice M1\n");
+    for(i=0;i<taille;i++)
+    {
+        for(j=0;j<taille;j++)
+        {
+            printf("M1[%d][%d] =",i,j);
+            scanf("%d",&M1[i][j]);
+        }
+    }
+
+    //initilisation de la matrice M2
+    printf("entrez les valeurs de la matrice M2\n");
+    for(i=0;i<taille;i++)
+    {
+        for(j=0;j<taille;j++)
+        {
+            printf("M2[%d][%d] =",i,j);
+            scanf("%d",&M2[i][j]);
+        }
+    }
+
+    //affichage de la matrice M1
+    printf("affichage matrice M1 :\n");
+    for(i=0;i<taille;i++)
+    {
+        for(j=0;j<taille;j++)
+        {
+            printf("%d",M1[i][j]);
+        }
+        printf("\n");
+    }
+
+    //affichage de la matrice M2
+    printf("affichage matrice M2 : \n");
+    for(i=0;i<taille;i++)
+    {
+        for(j=0;j<taille;j++)
+        {
+            printf("%d",M2[i][j]);
+        }
+        printf("\n");
+    }
+
+    //calcul du maximum des 2 images
+    for(i=0;i<taille;i++)
+    {
+        for(j=0;j<taille;j++)
+        {
+            if (M1[i][j]==0 && M2[i][j]==0)
+            {
+                M3[i][j]=0;
+            }
+            else
+            {
+                M3[i][j]=1;
+            }
+        }
+    }
+
+    //affichage de la matrice M3
+    printf("affichage matrice M3 : \n");
+    for(i=0;i<taille;i++)
+    {
+        for(j=0;j<taille;j++)
+        {
+            printf("%d   ",M3[i][j]);
+        }
+        printf("\n");
+    }
+
+    //fin
     return 0;
 }
