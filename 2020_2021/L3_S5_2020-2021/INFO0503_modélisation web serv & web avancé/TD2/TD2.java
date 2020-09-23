14fae87d89f76a8866c5455ee09b151b207530c6
broussk <49191931+broussk@users.noreply.github.com>
2020-09-23 18:32:05 +0200

    update TD info 503

--- 2020_2021/L3_S5_2020-2021/INFO0503_modélisation web serv & web avancé/TD2/TD2.java
+++ 2020_2021/L3_S5_2020-2021/INFO0503_modélisation web serv & web avancé/TD2/TD2.java
@@ -3,4 +3,52 @@
  * TONNELLE Nathan
  */
 
-//question 1 
+//question 5
+Class Serveur{
+    private Vector<String> iduse;
+    private int pont = 15000;
+    private String adresse;
+    
+    public static void main(String[] arg){
+        serverSocket ssoket = new serverSocket(port);
+        Socket s = ssocket.accept();
+        Scanner scan = new Scanner (s.getInputScanner);
+        String line = scan.nextLine();
+        /*
+         * "00"
+         * "10"
+         */
+        switch (line.substring(0,2)) {
+            case "00":
+                renvoie sur la socket la réponse;
+                enregistrer utilisateur;
+                break;
+            //...
+            case "10" :
+                break;
+            default:
+                break;
+        }
+        scan.close();
+    }
+}
+
+public class Client{
+    private String adUSer;
+    private int port;
+    public void main(String[] arg){
+        client c = NULL;
+        socket s = NULL;
+        scanner scan = NULL;
+        c = new client("mess",3128);
+        s = new socket("localhost", getPort());
+        s.connect();
+        bufferReader entree = NULL;
+        ObjectInputStream sortie = 
+        entree = new BufferReader(new InputStreamReader);
+        sortie = new ObjectInputStream(socket.getOutputStream);
+        String retour.mess = entree readLine();
+        sortie.writeObject(m);
+
+    }
+}
