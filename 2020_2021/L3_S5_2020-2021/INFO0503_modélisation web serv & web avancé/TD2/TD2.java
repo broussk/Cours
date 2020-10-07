3bfb0a68ecee3f9767cfa00517fe28abd024cb68
broussk <49191931+broussk@users.noreply.github.com>
2020-10-07 11:03:43 +0200

    td info503

--- 2020_2021/L3_S5_2020-2021/INFO0503_modélisation web serv & web avancé/TD2/TD2.java
+++ 2020_2021/L3_S5_2020-2021/INFO0503_modélisation web serv & web avancé/TD2/TD2.java
@@ -1,10 +1,18 @@
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import jdk.internal.jline.internal.InputStreamReader;
+import sun.net.InetAddressCachePolicy;
+
 /*
  * TD2 INFO0503
  * TONNELLE Nathan
  */
 
 //question 5
-Class Serveur{
+class Serveur{
     private Vector<String> iduse;
     private int pont = 15000;
     private String adresse;
@@ -50,5 +58,57 @@ public class Client{
         String retour.mess = entree readLine();
         sortie.writeObject(m);
 
+    }
+}
+
+public class TestServeurThreadTCP extends Thread{
+    final static int port = 66666;
+    private Socket socket;  //annonciation de la class threadTCP 
+    public static void main(String[] args){
+        ServeurSocket ServeurS = new ServeurSocket(port);
+        while(time){
+            socket = ServeurS.accept();
+            Thread t = new ThreadTCP(socket);
+            t.stort();
+        }
+    }
+}
+
+//classe :threadTCP
+public void run(){
+    traitement();
+}
+public void traitement(){
+    String toto = "";
+    String revient = "";
+    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(
+     new InputStreamReader(socket.getInputStream()));
+    toto = in.readLine();
+    switch(toto.substring(0,2)){
+        case "01" :
+            revient = choixIdentifiant();
+            break;
+        ...
+    }
+    PrintStream out = new PrintStream(socket.getOutputStream());
+    out.println(revient);
+    socket.close();
+}
+
+public class clientTCP{
+    final static int port = 6666;
+    public static void main(String[] args){
+        Socket s;
+        String t;
+        InetAddress ip = new InetAddress(getByName(args[0]));
+        s = new Socket(ip, port);
+        BefferedReader in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream()));
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        preparerTram(args[1], args[2], args[3], (-));
+        out.println(t);
+        String ret = ip.readLine();
+        System.out.println(ret);
     }
 }
