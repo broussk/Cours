37b52ef3c8f9ea9647290c6d50fa13902dd79725
broussk <49191931+broussk@users.noreply.github.com>
2020-10-21 16:07:28 +0200

    Update TD3.java

--- 2020_2021/L3_S5_2020-2021/INFO0503_modélisation web serv & web avancé/TD/TD3/TD3.java
+++ 2020_2021/L3_S5_2020-2021/INFO0503_modélisation web serv & web avancé/TD/TD3/TD3.java
@@ -9,7 +9,7 @@ class ThreadMessagerie
 public class ThreadMessagerie extends Thread {
     private Socket s;
     private MessageAgregation msgA;
-    private MessageEchangeListe messageEchangeListe;
+    private Vector<MessageEchangeListe> mg;
 
     public ThreadMessagerie(Socket s, MessageAgregation msgA){
         this.s = s;
@@ -23,6 +23,24 @@ public class ThreadMessagerie extends Thread {
         OutpuStream os = new OutpuStream(s.getOuputStream);
         ObjectOutputStream oos = new ObjectOutputStream(os);
 
-        
+        Vector<MessageEchange> reponse;
+
+        while{
+            msgA = oos.read(Object());
+            if(msgA.getlist()[0].getIdMsg() == 0){
+                mg.add(msgA.getlist[0]);
+                reponse.setID(1);
+            }
+            else{        
+                for(int i=0; i<msg.lenght; i++){
+                    if(mg[i].getDestinataire() == msgA.getListe()[0].getExpediteur()){
+                        reponse.add(mg[i]);
+                    }
+                    reponse.setId(1);
+                }
+                oos.writeObject(reponse);
+            }
+        }
+        s.close();
     }
 }
